a4ee6b44da7f52428866cbd80e797657b75db1cf
maxM808s <max.matarasso@gmail.com>
2026-08-17 20:13:44 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ window.GOAL_CUP = {
 
   databaseURL: "https://soccergame-6564d-default-rtdb.europe-west1.firebasedatabase.app",
 
-  apiKey: "AIzaSyDRDEN7N3MfSJmkICBDqLHFyAu5kb8X6sA",
+  apiKey: "new AIzaSyC--EVh00HWkSyiVzlHdNP9WUger5lUyy4",
 
   room: "cup-7fk29xqp"
 
